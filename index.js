571336a7a939b6121164f23de07f2e875c95176c
vaibhav2332 <vaibhavprajapati369@gmail.com>
2026-03-06 20:54:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-const {fork} = require('child_process');
-const chalk = require('chalk')
+const { fork } = require('child_process');
+const chalk = require('chalk');
 
-async function start(){
-const child = fork('./main.js')
-child.on("message",msg=>{
-console.log('child to parent =>',msg)
-})
+async function start() {
+    const child = fork('./main.js');
+    
+    child.on("message", msg => {
+        console.log('child to parent =>', msg);
+    });
 
-child.on("close",(anu)=>{
-console.log(chalk.black(chalk.bgRed(`Bot is auto restarting..`)))
-start()
-})
+    child.on("close", (anu) => {
+        console.log(chalk.black(chalk.bgRed(`Bot is auto restarting..`)));
+        start();
+    });
 
-child.on("exit",(anu2)=>{
-})
+    child.on("exit", (anu2) => {
+    });
 
+    // --- NEW ANTI-SLEEP FIX ---
+    // Forcefully kill and restart the process every 1 hours 
+    // to refresh the silent WhatsApp WebSocket drop.
+    setTimeout(() => {
+        console.log(chalk.yellow("Scheduled restart: Refreshing WhatsApp connection to prevent silent sleep..."));
+        child.kill(); 
+    }, 1 * 60 * 60 * 1000); // 1 hours in milliseconds
 }
-start()
+
+start();
